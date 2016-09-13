6314095acd9b4332570518facceca0ab958d87c3
derrick.brown@yellowdogtech.com <derrick.brown@yellowdogtech.com>
2016-09-13 15:22:00 -0600

    initial revision

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Blah, Blah, Blah</p>
+      </div>
+    );
+
+  }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>PSAdmin</h1>
+        <p>React, Redux, React-Router in ES6</p>
+        <Link  to="about" className="btn btn-primary btn-lg">Learn more</Link>
+      </div>
+    );
+  }
+
+}
+
+export default HomePage;
+
